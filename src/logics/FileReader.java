ffd5b8efe8de6aece01198301c466fec85301dbb
Natalya Selyunina <seluninanv@mail.ru>
2023-08-30 22:02:12 +0300

    feat : fields players and SEPARATOR

---
+++ src/logics/FileReader.java
@@ -0,0 +1,18 @@
+package logics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReader {
+  private List<Player> players = new ArrayList<>();
+
+  private static final String SEPARATOR = ";";
+
+  /**
+   * Чтение данных из файла
+   *
+   */
+  public void readingFromFile() {
+
+  }
+}
