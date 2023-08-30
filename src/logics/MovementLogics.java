73198f468dd400df7fb23563d4bb63ba4c6afae7
Natalya Selyunina <seluninanv@mail.ru>
2023-08-30 04:07:20 +0300

    fix : checkMovement()

--- src/logics/MovementLogics.java
+++ src/logics/MovementLogics.java
@@ -3,25 +3,40 @@ package logics;
 import graphics.code.Board;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.print.DocFlavor.READER;
 import javax.swing.JPanel;
 
 public class MovementLogics extends JPanel {
 
   private boolean isWhitesTurn = true;
+  private final int WHITECHECKER = 1;
+  private final int BLACKCHECKER = 2;
 
   public boolean checkMovement(int[][] board, int row, int col, int selectedRow, int selectedCol) {
-    if(isWhitesTurn) {
-      if (col == (selectedCol + 1) | col == (selectedCol - 1) && row == (selectedRow + 1)) {
+    if (isWhitesTurn) {
+      if (col == (selectedCol + 1) | col == (selectedCol - 1) && row == (selectedRow + 1)
+          && board[row][col] == 0) {
+        if (board[row][col] == 0) {
+          isWhitesTurn = false;
+          return true;
+        }
+      } else if (col == (selectedCol + 2) | col == (selectedCol - 2) && row == (selectedRow + 2)
+          && board[row][col] == BLACKCHECKER) {
         isWhitesTurn = false;
         return true;
       }
     }
 
     if (!isWhitesTurn) {
-      if (col == (selectedCol + 1) | col == (selectedCol - 1) && row == (selectedRow - 1)) {
+      if (col == (selectedCol + 1) | col == (selectedCol - 1) && row == (selectedRow - 1)
+          && board[row][col] == 0) {
         isWhitesTurn = true;
         return true;
       }
+    } else if (col == (selectedCol + 2) | col == (selectedCol - 2) && row == (selectedRow - 2)
+        && board[row][col] == WHITECHECKER) {
+      isWhitesTurn = true;
+      return true;
     }
 
     return false;
