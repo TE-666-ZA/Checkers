5c8248a60da66cd852219592b9331b533414b1d0
Natalya Selyunina <seluninanv@mail.ru>
2023-08-30 04:34:42 +0300

    fix : checkMovement() feat : canKill()

--- src/logics/MovementLogics.java
+++ src/logics/MovementLogics.java
@@ -16,12 +16,10 @@ public class MovementLogics extends JPanel {
     if (isWhitesTurn) {
       if (col == (selectedCol + 1) | col == (selectedCol - 1) && row == (selectedRow + 1)
           && board[row][col] == 0) {
-        if (board[row][col] == 0) {
-          isWhitesTurn = false;
-          return true;
-        }
-      } else if (col == (selectedCol + 2) | col == (selectedCol - 2) && row == (selectedRow + 2)
-          && board[row][col] == BLACKCHECKER) {
+        isWhitesTurn = false;
+        return true;
+
+      } else if (canKill(board, row, col, selectedRow, selectedCol)) {
         isWhitesTurn = false;
         return true;
       }
@@ -33,12 +31,30 @@ public class MovementLogics extends JPanel {
         isWhitesTurn = true;
         return true;
       }
-    } else if (col == (selectedCol + 2) | col == (selectedCol - 2) && row == (selectedRow - 2)
-        && board[row][col] == WHITECHECKER) {
+    } else if (canKill(board, row, col, selectedRow, selectedCol)) {
       isWhitesTurn = true;
       return true;
     }
 
+    return false;
+  }
+
+  public boolean canKill(int[][] board, int row, int col, int selectedRow, int selectedCol) {
+    if (board[selectedRow + 1][selectedCol + 1] == BLACKCHECKER
+        || board[selectedRow + 1][selectedCol - 1] == BLACKCHECKER) {
+      if (col == (selectedCol + 2) | col == (selectedCol - 2) && row == (selectedRow + 2)
+          && board[row][col] == 0) {
+        return true;
+      }
+    }
+
+    if (board[selectedRow - 1][selectedCol + 1] == WHITECHECKER
+        || board[selectedRow - 1][selectedCol - 1] == WHITECHECKER) {
+      if (col == (selectedCol + 2) | col == (selectedCol - 2) && row == (selectedRow - 2)
+          && board[row][col] == 0) {
+        return true;
+      }
+    }
     return false;
   }
 
