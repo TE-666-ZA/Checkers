aad84e93d4644afc2a01e3ac1756d07e05f648bd
Natalya Selyunina <seluninanv@mail.ru>
2023-08-30 22:19:28 +0300

    feat : readingFromFile() with comment, add field ourFile

--- src/logics/FileReader.java
+++ src/logics/FileReader.java
@@ -1,18 +1,45 @@
 package logics;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class FileReader {
-  private List<Player> players = new ArrayList<>();
 
+  private String ourFile = "res/playersStatistics.csv";
+  private List<Player> players = new ArrayList<>();
   private static final String SEPARATOR = ";";
 
   /**
    * Чтение данных из файла
    *
    */
-  public void readingFromFile() {
+  public void readingFromFile() throws IOException {
+    File file = new File(ourFile);
+    if (!file.exists() || file.length() == 0) {
+      return;
+    }
+
+    Scanner scanner = new Scanner(file);
+    while (scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] cells = line.split(SEPARATOR);
+      String name = cells[0];
+      int numberOfVictories = Integer.parseInt(cells[1]);
+      int numberOfDefeats = Integer.parseInt(cells[2]);
+      Player player = new Player(name);
+      players.add(player);
+    }
+    scanner.close();
+  }
+
+  /**
+   * Запись данных в файл
+   *
+   */
+  public void writingToFile() {
 
   }
 }
