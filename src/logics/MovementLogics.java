29acb101b182f6b44fb5472901ec07d27cbb0762
Natalya Selyunina <seluninanv@mail.ru>
2023-08-31 20:51:44 +0300

    fix : add comment leftBorderLogicForBlack()

--- src/logics/MovementLogics.java
+++ src/logics/MovementLogics.java
@@ -53,18 +53,18 @@ public class MovementLogics extends JPanel {
   }
 
   /**
-   * The opportunity to cut down the opponent's checker
+   * The opportunity to eat the opponent's checker
    *
    * @param board       playing field
-   * @param row         the position of the row of the cell of the field where we want to move the
-   *                    checker
-   * @param col         the position of the column of the cell of the field where we want to move
-   *                    the checker
-   * @param selectedRow the position of the row of the cell of the field from where we want to move
-   *                    the checker
-   * @param selectedCol the position of the column of the cell of the field from where we want to
-   *                    move the checker
-   * @return
+   * @param row         the position of the row of the field cell where we will get to if we can eat
+   *                    the opponent's checker
+   * @param col         the position of the column of the cell of the field where we will get if we
+   *                    can eat the opponent's checker
+   * @param selectedRow the position of the row of the cell of the field where the checker with
+   *                    which we want to eat the opponent's checker is located
+   * @param selectedCol the position of the column of the cell of the field where the checker with
+   *                    which we want to eat the opponent's checker is located
+   * @return true if the white checker can eat the opponent's black checker, and false if it can't
    */
   private boolean canKill(int[][] board, int row, int col, int selectedRow, int selectedCol) {
     if (checkerMove == WHITECHECKER) {
@@ -115,7 +115,21 @@ public class MovementLogics extends JPanel {
     return false;
   }
 
-
+  /**
+   * Checks the possibility of eating the opponent's black checkers with a white checker located in
+   * the left most column
+   *
+   * @param board       playing field
+   * @param row         the position of the row of the cell of the playing field, where the white
+   *                    checker will fall if it eats the opponent's checker
+   * @param col         the position of the column of the cell of the playing field, where the white
+   *                    checker will fall if it eats the opponent's checker
+   * @param selectedRow the position of the cell row of the white checker field, which is located in
+   *                    the left most column and which plans to eat the opponent's black checker
+   * @param selectedCol the position of the left most column where the white checker is located,
+   *                    which plans to eat the opponent's black checker
+   * @return true if the white checker can eat the opponent's black checker, and false if it can't
+   */
   private boolean leftBorderLogicForWhite(int[][] board, int row, int col, int selectedRow,
       int selectedCol) {
     if (row == (selectedRow + 2) && col == (selectedCol + 2) && isTargetCellFree(board, row, col)
@@ -127,6 +141,21 @@ public class MovementLogics extends JPanel {
     return false;
   }
 
+  /**
+   * Checks the possibility of eating the opponent's white checkers with a black checker located in
+   * the left most column
+   *
+   * @param board       playing field
+   * @param row         the position of the row of the cell of the playing field, where the black
+   *                    checker will fall if it eats the opponent's white checker
+   * @param col         the position of the column of the cell of the playing field, where the black
+   *                    checker will fall if it eats the opponent's white checker
+   * @param selectedRow the position of the cell row of the black checker field, which is located in
+   *                    the left most column and which plans to eat the opponent's white checker
+   * @param selectedCol the position of the left most column where the black checker is located
+   *                    which plans to eat the opponent's white checker
+   * @return true if the black checker can eat the opponent's white checker, and false if it can't
+   */
   private boolean leftBorderLogicForBlack(int[][] board, int row, int col, int selectedRow,
       int selectedCol) {
     if (row == (selectedRow - 2) && col == (selectedCol + 2) && isTargetCellFree(board, row, col)
@@ -160,6 +189,17 @@ public class MovementLogics extends JPanel {
     return false;
   }
 
+  /**
+   * Checks that the cell of the playing field is free
+   *
+   * @param board playing field
+   * @param row   the position of the row of the cell of the playing field for the presence of other
+   *              checkers there
+   * @param col   the position of the column of the cell of the playing field for the presence of
+   *              other checkers there
+   * @return true if the cell of the playing field is free, and false if there is another checker on
+   * the cell
+   */
   private boolean isTargetCellFree(int[][] board, int row, int col) {
     return board[row][col] == 0;
   }
@@ -168,10 +208,26 @@ public class MovementLogics extends JPanel {
     return checkerMove;
   }
 
+  /**
+   * Checks that the checker we want to go to is in the left most column
+   *
+   * @param selectedCol the position of the column of the cell of the playing field, where the
+   *                    checker is located, which we want to make a move
+   * @return true if the checker is in the left most table of the playing field, and false if the
+   * checker is in any other table
+   */
   private boolean isLeftBorderChecker(int selectedCol) {
     return selectedCol == 0;
   }
 
+  /**
+   * Checks that the checker we want to go to is in the right most column
+   *
+   * @param selectedCol the position of the column of the cell of the playing field, where the
+   *                    checker is located, which we want to make a move
+   * @return true if the checker is in the right most table of the playing field, and false if the
+   * checker is in any other table
+   */
   private boolean isRightBorderChecker(int selectedCol) {
     return selectedCol == 7;
   }
