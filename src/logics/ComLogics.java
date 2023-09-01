e6c25ec353f85c29d731646fc3b50c82561798c0
Natalya Selyunina <seluninanv@mail.ru>
2023-09-02 00:55:08 +0300

    fix : class ComLogics implements MovementLogics

--- src/logics/ComLogics.java
+++ src/logics/ComLogics.java
@@ -1,6 +1,6 @@
 package logics;
 
-public class ComLogics {
+public class ComLogics implements MovementLogics {
 
   private Player player;
 
@@ -12,8 +12,6 @@ public class ComLogics {
     this.player = new Player(name);
   }
 
-
-
   public boolean isFirstMoveMakesCom() {
     if (this.player.getSelectedCheckerColor() == 1) {
       return true;
@@ -23,5 +21,99 @@ public class ComLogics {
 
   public Player getPlayer() {
     return player;
+  }
+
+  @Override
+  public boolean checkMovement(int[][] board, int row, int col, int selectedRow, int selectedCol) {
+    return false;
+  }
+
+  @Override
+  public boolean canKill(int[][] board, int row, int col, int selectedRow, int selectedCol) {
+    return false;
+  }
+
+  @Override
+  public boolean leftBorderLogicForWhite(int[][] board, int row, int col, int selectedRow,
+      int selectedCol) {
+    return false;
+  }
+
+  @Override
+  public boolean leftBorderLogicForBlack(int[][] board, int row, int col, int selectedRow,
+      int selectedCol) {
+    return false;
+  }
+
+  @Override
+  public boolean rightBorderLogicForWhite(int[][] board, int row, int col, int selectedRow,
+      int selectedCol) {
+    return false;
+  }
+
+  @Override
+  public boolean rightBorderLogicForBlack(int[][] board, int row, int col, int selectedRow,
+      int selectedCol) {
+    return false;
+  }
+
+  @Override
+  public boolean isTargetCellFree(int[][] board, int row, int col) {
+    return false;
+  }
+
+  @Override
+  public int getCheckerMove() {
+    return 0;
+  }
+
+  @Override
+  public boolean isLeftBorderChecker(int selectedCol) {
+    return false;
+  }
+
+  @Override
+  public boolean isRightBorderChecker(int selectedCol) {
+    return false;
+  }
+
+  @Override
+  public boolean isWhiteCheckerResetToZero() {
+    return false;
+  }
+
+  @Override
+  public boolean isBlackCheckerResetToZero() {
+    return false;
+  }
+
+  @Override
+  public void minusOneWhiteChecker() {
+
+  }
+
+  @Override
+  public void minusOneBlackChecker() {
+
+  }
+
+  @Override
+  public void checkWhiteVictory() {
+
+  }
+
+  @Override
+  public void checkBlackVictory() {
+
+  }
+
+  @Override
+  public void gameOverWithWhiteVictory() {
+
+  }
+
+  @Override
+  public void gameOverWithBlackVictory() {
+
   }
 }
