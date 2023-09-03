94e58ad97911a56e182c7f30cc380a89c3d87e49
Natalya Selyunina <seluninanv@mail.ru>
2023-09-03 19:42:12 +0300

    fix : throws IOexception

--- src/logics/FileReader.java
+++ src/logics/FileReader.java
@@ -15,37 +15,45 @@ public class FileReader {
   /**
    * Reading data from a file
    */
-  public void readingFromFile() throws IOException {
-    File file = new File(ourFile);
-    if (!file.exists() || file.length() == 0) {
-      return;
-    }
+  public void readingFromFile() {
+    try {
+      File file = new File(ourFile);
+      if (!file.exists() || file.length() == 0) {
+        return;
+      }
 
-    Scanner scanner = new Scanner(file);
-    while (scanner.hasNextLine()) {
-      String line = scanner.nextLine();
-      String[] cells = line.split(SEPARATOR);
-      String name = cells[0];
-      int numberOfVictories = Integer.parseInt(cells[1]);
-      int numberOfDefeats = Integer.parseInt(cells[2]);
-      Player player = new Player(name);
-      players.add(player);
+      Scanner scanner = new Scanner(file);
+      while (scanner.hasNextLine()) {
+        String line = scanner.nextLine();
+        String[] cells = line.split(SEPARATOR);
+        String name = cells[0];
+        int numberOfVictories = Integer.parseInt(cells[1]);
+        int numberOfDefeats = Integer.parseInt(cells[2]);
+        Player player = new Player(name);
+        players.add(player);
+      }
+      scanner.close();
+    } catch (IOException e) {
+      System.err.println("Statistic file could not be find");
     }
-    scanner.close();
   }
 
   /**
    * Writing data to a file
    */
-  public void writingToFile() throws IOException {
-    FileWriter fileWriter = new FileWriter(ourFile);
+  public void writingToFile() {
+    try {
+      FileWriter fileWriter = new FileWriter(ourFile);
 
-    for (Player player : players) {
-      String result = player.getName() + SEPARATOR + player.getNumberOfVictories() + SEPARATOR
-          + player.getNumberOfDefeats();
-      fileWriter.write(result + "\n");
+      for (Player player : players) {
+        String result = player.getName() + SEPARATOR + player.getNumberOfVictories() + SEPARATOR
+            + player.getNumberOfDefeats();
+        fileWriter.write(result + "\n");
+      }
+      fileWriter.close();
+    } catch (IOException e) {
+      System.err.println("Statistic file could not be find");
     }
-    fileWriter.close();
   }
 
   public HashSet<Player> getPlayers() {
