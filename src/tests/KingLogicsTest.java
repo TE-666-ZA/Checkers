91b804af5daaad14a976f951c1bf9d827516d1c2
Natalya Selyunina <seluninanv@mail.ru>
2023-09-12 04:13:28 +0300

    feat : KingLogicsTest

---
+++ src/tests/KingLogicsTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import logics.KingLogics;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class KingLogicsTest {
+  private KingLogics kingLogics;
+  @BeforeEach
+  public void setUp() {
+    this.kingLogics = new KingLogics();
+  }
+
+  @Test
+  public void differentRowDown() {
+    int selectedRow = 3;
+    int row = 7;
+    int differentRow = kingLogics.differentRow(selectedRow, row);
+    assertEquals(4, differentRow);
+  }
+
+  @Test
+  public void differentRowUp() {
+    int selectedRow = 7;
+    int row = 3;
+    int differentRow = kingLogics.differentRow(selectedRow, row);
+    assertEquals(4, differentRow);
+  }
+}
